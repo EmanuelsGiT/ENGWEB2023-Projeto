7eb46527b631481262969937efa9ad632a28ee26
DinisEstrada <dinis.estrada37@gmail.com>
2023-06-20 00:05:57 +0100

    script tratamento dataset

--- filiacao.py
+++ filiacao.py
@@ -2,16 +2,33 @@ import re
 import json
 
 # Regex pattern to capture names
-regex_pattern = r'Filiação:\s(.*?)(?:\s+e|,\w)'
+regex_pattern_filiacao = r'Filiação:\s(.*?)(?:\s+e|,\w)'
+regex_pattern_related_material = r'Proc\.(\d+)'
 
-def process_entry(entry):
-    scope_content = entry["ScopeContent"]
-    match = re.search(regex_pattern, scope_content)
-    if match:
-        filiacao = match.group(0)
-        entry["Filiacao"] = filiacao[len("Filiação: "):-2]
+def process_entry(entry, data):
+    scope_content = entry.get("ScopeContent", "")
+    match_filiacao = re.search(regex_pattern_filiacao, scope_content)
+    if match_filiacao:
+        filiacao = match_filiacao.group(0)
+        entry["Filiacao"] = []
+
+        for entry_data in data:
+            if "ScopeContent" in entry_data and filiacao in entry_data["ScopeContent"]:
+                entry["Filiacao"].append(entry_data["_id"])
+
     else:
-        entry["Filiacao"] = None
+        entry["Filiacao"] = []
+
+    related_material = entry.get("RelatedMaterial", "")
+    match_related_material = re.findall(regex_pattern_related_material, related_material)
+    if match_related_material:
+        for match in match_related_material:
+            for entry_data in data:
+                if "UnitId" in entry_data and entry_data["UnitId"] == match:
+                    entry["Filiacao"].append(entry_data["_id"])
+                    break
+
+    entry["Filiacao"] = list(set(entry["Filiacao"]))
 
 
 # Function to process the JSON file and write new data to a file
@@ -20,7 +37,7 @@ def process_json_file(input_file_path, output_file_path):
         data = json.load(input_file)
 
     for entry in data:
-        process_entry(entry)
+        process_entry(entry, data)
 
     with open(output_file_path, "w", encoding="utf-8") as output_file:
         json.dump(data, output_file, indent=4, ensure_ascii=False)
